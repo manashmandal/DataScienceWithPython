53d82eb48171fbfe77614fb8a106632d7e3c183c
Manash Mandal <manashmndl@gmail.com>
2015-03-10 23:58:24 -0400

    Create plottingwithpylab.py

---
+++ plottingwithpylab.py
@@ -0,0 +1,13 @@
+import pylab
+
+pylab.figure(1)
+pylab.plot([1, 2, 3, 4], [1, 2, 3, 4])
+pylab.figure(2)
+pylab.plot([1, 4, 2, 3], [5, 6, 7, 8])
+
+pylab.savefig('Fuchking figure')
+
+pylab.figure(1)
+pylab.plot([5, 6, 10, 3])
+pylab.savefig('Fuchked!')
+
