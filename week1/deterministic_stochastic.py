0f707e8e2fb021c5d4cf13db45d70fce2e872aa6
Manash Mandal <manashmndl@gmail.com>
2015-03-14 05:38:20 -0400

    Update and rename deterministicnumber.py to deterministic_stochastic.py

---
+++ week1/deterministic_stochastic.py
@@ -0,0 +1,23 @@
+import random
+
+def deterministicNumber():
+  return 10
+
+#Sol 1
+
+def stochasticNumber():
+  return 2 * random.randint(5, 10)
+  
+# Sol2  
+
+def stochasticNumber():
+  return random.randrange(10, 22, 2)
+
+#Sol 3
+
+def stochasticNumber():
+  x = random.randint(10, 22)
+  if (x % 2 == 0):
+    return x
+  else:
+    return x-1
--- week1/deterministicnumber.py
+++
@@ -1,2 +0,0 @@
-def deterministicNumber():
-  return 10
